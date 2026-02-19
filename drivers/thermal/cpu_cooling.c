f6ad5aa73d0ea1839a04043cffdd63333efd4fd6
duckyduckG <duckyduckg65@gmail.com>
2026-02-19 16:32:42 +0500

    thermal: Import Xiaomi changes
    * vayu-r-oss
    * drm related changes are dropped

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -89,11 +89,42 @@ struct cpufreq_cooling_device {
 	struct list_head node;
 	struct time_in_idle *idle_time;
 	struct freq_qos_request qos_req;
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	struct thermal_cooling_device *cdev;
+#endif
 };
 
 static DEFINE_IDA(cpufreq_ida);
 static DEFINE_MUTEX(cooling_list_lock);
 static LIST_HEAD(cpufreq_cdev_list);
+
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+void cpu_limits_set_level(unsigned int cpu, unsigned int max_freq)
+{
+	struct cpufreq_cooling_device *cpufreq_cdev;
+	struct thermal_cooling_device *cdev;
+	unsigned int cdev_cpu;
+	unsigned int level;
+
+	list_for_each_entry(cpufreq_cdev, &cpufreq_cdev_list, node) {
+		sscanf(cpufreq_cdev->cdev->type, "thermal-cpufreq-%d", &cdev_cpu);
+		if (cdev_cpu == cpu) {
+			for (level = 0; level <= cpufreq_cdev->max_level; level++) {
+				int target_freq = cpufreq_cdev->freq_table[level].frequency;
+				if (max_freq >= target_freq) {
+					cdev = cpufreq_cdev->cdev;
+					if (cdev)
+						cdev->ops->set_cur_state(cdev, level);
+
+					break;
+				}
+			}
+
+			break;
+		}
+	}
+}
+#endif
 
 /* Below code defines functions to be used for cpufreq as cooling device */
 
@@ -632,6 +663,10 @@ __cpufreq_cooling_register(struct device_node *np,
 						  cooling_ops);
 	if (IS_ERR(cdev))
 		goto remove_qos_req;
+
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+	cpufreq_cdev->cdev = cdev;
+#endif
 
 	mutex_lock(&cooling_list_lock);
 	list_add(&cpufreq_cdev->node, &cpufreq_cdev_list);
--- include/linux/cpu_cooling.h
+++ include/linux/cpu_cooling.h
@@ -47,6 +47,10 @@ void cpufreq_cooling_unregister(struct thermal_cooling_device *cdev)
 }
 #endif	/* CONFIG_CPU_THERMAL */
 
+#if defined(CONFIG_MACH_XIAOMI_SM8150)
+void cpu_limits_set_level(unsigned int cpu, unsigned int max_freq);
+#endif
+
 #if defined(CONFIG_THERMAL_OF) && defined(CONFIG_CPU_THERMAL)
 /**
  * of_cpufreq_cooling_register - create cpufreq cooling device based on DT.
